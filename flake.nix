0a525b50ff073951c9bada39c012a2bfef729a4d
Mads Wrang <madscwn@gmail.com>
2025-11-26 16:05:57 +0100

    initial commit: rust flake setup

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Rust flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          rust-analyzer
+          clippy
+        ];
+      };
+    };
+}
